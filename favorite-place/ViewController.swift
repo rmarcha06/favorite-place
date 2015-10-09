2e869432bf7ba533807a4f41d393b4e250690f98
Ramiro Martinez Chavez <ramiromartinezchavez@MacBook-Pro-de-Ramiro.local>
2015-10-09 13:21:03 -0500

    Added all files

---
+++ favorite-place/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  favorite-place
+//
+//  Created by Ramiro Martinez Chavez on 08/10/15.
+//  Copyright © 2015 Ramiro Martinez Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var moreBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        moreBtn.layer.cornerRadius = 2.0
+        
+    }
+
+    
+
+
+}
+
