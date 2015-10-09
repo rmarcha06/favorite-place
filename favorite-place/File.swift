760849c7b38a223193b0cf740129ffb408c2a37a
Ramiro Martinez Chavez <ramiromartinezchavez@macbook-pro-de-ramiro.local>
2015-10-09 13:34:24 -0500

    archivo agregado

---
+++ favorite-place/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  favorite-place
+//
+//  Created by Ramiro Martinez Chavez on 09/10/15.
+//  Copyright © 2015 Ramiro Martinez Chavez. All rights reserved.
+//
+
+import Foundation
